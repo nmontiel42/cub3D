64fcf5d17e4835e0396d873695de57cc99ccdacf
Nekane <125030234+nmontiel42@users.noreply.github.com>
2024-05-01 18:08:36 +0200

    Add files via upload

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmontiel <montielarce9@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 13:27:32 by nmontiel          #+#    #+#             */
+/*   Updated: 2024/05/01 15:32:28 by nmontiel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	s1_length;
+	char	*result;
+	int		i;
+
+	if (!s1)
+	{
+		s1 = ft_calloc(1, sizeof(char));
+		if (!s1)
+			return (free(s1), s1 = NULL, NULL);
+	}
+	if (!s1 || !s2)
+		return (free(s1), s1 = NULL, NULL);
+	s1_length = ft_strlen(s1);
+	result = (char *)ft_calloc((s1_length + ft_strlen(s2) + 1),
+			sizeof(char));
+	if (!result)
+		return (free(s1), s1 = NULL, NULL);
+	i = -1;
+	while (s1[++i])
+		result[i] = s1[i];
+	i = -1;
+	while (s2[++i])
+		result[s1_length + i] = s2[i];
+	result[s1_length + i] = '\0';
+	return (free(s1), s1 = NULL, result);
+}
